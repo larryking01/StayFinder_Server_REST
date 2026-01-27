d3013953f54e797749843d8fb4d539f3ed22ddba
= <larryking8118@gmail.com>
2026-01-27 15:01:22 +0000

    feat(reviews): implement insert reviews logic for authenticated users only

--- Routes/reviews.routes.js
+++ Routes/reviews.routes.js
@@ -1,5 +1,9 @@
 
 import express from 'express';
+import createUserSupabaseClient from '../configs/supabase.user.js';
+
+
+
 
 
 
@@ -7,8 +11,49 @@ const reviewsRouter = express.Router();
 
 
 // POST requests
-reviewsRouter.post('/add-new-review/:user', ( req, res ) => {
-    res.json("add new reviews route reached")
+reviewsRouter.post('/add-new-review', async ( req, res ) => {
+    try {
+        let supabaseUser = createUserSupabaseClient( req )
+
+        let reviewData = {
+            user_name: req.body.user_name,
+            hotel_id: req.body.hotel_id,
+            hotel_name: req.body.hotel_name,
+            rating: req.body.rating,
+            review_title: req.body.review_title,
+            review_content: req.body.review_content,
+            is_visible: req.body.is_visible,
+            verified_stay: req.body.verified_stay,
+        }
+
+        const { data, error } = await supabaseUser.from("Reviews").insert([ reviewData ]).select().single()
+
+        if( error ) {
+            res.status(500).json({
+                success: false,
+                status: 500,
+                message: "Failed to add new review...",
+                error: error
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                status: 200,
+                message: "Review added successfully...",
+                data: data
+            })
+        }
+    }
+    catch( err ) {
+        res.status(500).json({
+            success: false,
+            status: 500,
+            message: "Internal server error...",
+            error: err
+        })
+    }
+    
 })
 
 
